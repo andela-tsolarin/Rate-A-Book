700b6b36effba56be7e08d4bf83258a900fa2382
Developer Without Borders <toni.edward@outlook.com>
2015-05-11 15:24:02 +0100

    Added search functionality

--- js/controllers/home.js
+++ js/controllers/home.js
@@ -2,14 +2,28 @@ app.controller("home", function ($scope, $window, $route, $book) {
 
   $scope.rootPath = globalVars.apiHost;
   $scope.books = [];
+  $scope.booksResult = [];
+  $scope.query = '';
 
   $book.getBooks()
     .success(function(data) {
       $scope.books = data;
+      $scope.booksResult = data;
     });
 
 
   $scope.search = function() {
-    $window.alert("Searching...");
+    $scope.booksResult = [];
+
+    for (var i = 0; i < $scope.books.length; i++) {
+      var book = $scope.books[i];
+      var q = $scope.query.toLowerCase();
+
+      if (book.title.toLowerCase().indexOf(q) != -1) {
+        console.log(book);
+        $scope.booksResult.push(book);
+      }
+    }
+
   }  
 });
