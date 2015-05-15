d27f29030d5828329c62343fdca43f9c3ad762bf
Developer Without Borders <toni.edward@outlook.com>
2015-05-15 15:17:55 +0100

    Github pages. Api var edit

--- js/globalvars.js
+++ js/globalvars.js
@@ -1,2 +1,3 @@
 ﻿var globalVars = {};
-globalVars.apiHost = "http://localhost/andela/Rate-A-Book/api";
+//globalVars.apiHost = "http://localhost/andela/Rate-A-Book/api";
+globalVars.apiHost = "http://andela-tsolarin.github.io/Rate-A-Book/api";
